81b4ff1582a747a50ec6a0687f5e5bbe11c36e0a
Naufal Bintang Brillian <155503895+brilliannaufal123@users.noreply.github.com>
2025-09-28 20:41:53 +0700

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,18 +90,83 @@ serviceButtons.forEach(button => {
 
 
 /*=============== COPY EMAIL IN CONTACT ===============*/
+const copyBtn = document.getElementById('container-btn'); 
+      copyEmail = document.getElementById('contact-email');
 
+copyBtn.addEventListener('click', () => {
+    navigator.clipboard.writeText(copyEmail.textContent).then(() => {
+        copyBtn.innerHTML = "Email copied <i class='ri-check-line'></i>";
+        setTimeout(() => {
+            copyBtn.innerHTML = "Copy Email <i class='ri-file-copy-line'></i>";
+        }, 2000);
+    })
+});
 
 /*=============== CURRENT YEAR OF THE FOOTER ===============*/ 
+const textYear = document.getElementById('footer-year'),
+      currentYear = new Date().getFullYear();
 
-
+      textYear.textContent = currentYear;
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+const scrollActive = () => {
+    const scrollY = window.scrollY;
 
+    sections.forEach(section =>{
+        const id = section.id,
+              top = section.offsetTop - 50,
+              height = section.offsetHeight,
+              link = document.querySelector('.nav__menu a[href*=' + id + ']');
+        if(!link) return;
+
+        link.classList.toggle('active-link', scrollY > top && scrollY <= top + height);
+    })
+
+}
+window.addEventListener('scroll', scrollActive);
 /*=============== CUSTOM CURSOR ===============*/
+const cursor = document.querySelector('.cursor');
+let mouseX = 0, mouseY = 0;
 
+const cursorMove = () => {
+    cursor.style.left = `${mouseX}px`; 
+    cursor.style.top = `${mouseY}px`; 
+    cursor.style.transform = 'translate(-50%, -50%)';
 
+    requestAnimationFrame(cursorMove);
+}
+
+document.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+
+cursorMove();
 /* Hide custom cursor on links */
+const a = document.querySelectorAll('a');
 
+a.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        cursor.classList.add('hide-cursor');
+    });
+    item.addEventListener('mouseleave', () => {
+        cursor.classList.remove('hide-cursor');
+    })
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 300,
+});
+
+sr.reveal('.home__image, .projects__container, work__container, .testimonials__container, .contact__container, .footer__container')
+sr.reveal('.home__data', {delay: 900, origin: 'bottom'})
+sr.reveal('.home__info', {delay: 1200, origin: 'bottom'})
+sr.reveal('.home__social, .home__cv', {delay: 1500})
+sr.reveal('.about__data', {origin: 'left'})
+sr.reveal('.about__image', {origin: 'right'})
+sr.reveal('.services__card', {interval: 100})
